d4a9cae6264876bb2f83ab16ac05a5a3be5815cf
Carl Uebelhart <carl@erutan.net>
2015-01-16 16:16:52 -0800

    query results cleaner, rough layout for actions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,9 +4,14 @@
   document.addEventListener('polymer-ready', function() {
     // foo
   });
+  
+  var tabs = document.querySelector('paper-tabs');
+  var pages = document.querySelector('core-pages');
+  
+  tabs.addEventListener('core-select',function(){
+    pages.selected = tabs.selected;
+  });
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
-
-
