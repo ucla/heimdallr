2f3f3bcf0a85c28936109ed1c3e00e23b4d53f85
Carl Uebelhart <carl@erutan.net>
2015-03-19 13:22:47 -0700

    simplify js load, update bower deps

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,24 @@
+/////////////////////////////////////////
+///     load & configure webshims
+/////////////////////////////////////////
+
+webshim.setOptions('forms', {
+  //set lazyCustomMessages to true
+  lazyCustomMessages: true,
+  //show custom styleable validation bubble
+  replaceValidationUI: true,
+  iVal: {
+    "recheckDelay": 200,
+  }
+});
+
+//start polyfilling
+webshim.polyfill('forms');
+
+/////////////////////////////////////////
+///           action tabs
+/////////////////////////////////////////
+
 (function (document) {
   'use strict';
 
