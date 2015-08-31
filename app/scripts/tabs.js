1284cf29c1a9c0977a28c9cf1805ff81f265c106
Alfonso Roman <aroman@it.ucla.edu>
2015-08-31 16:45:26 -0700

    fixing polymer errors

--- app/scripts/tabs.js
+++ app/scripts/tabs.js
@@ -8,9 +8,12 @@
   var tabs = document.querySelector('paper-tabs');
   var pages = document.querySelector('core-pages');
 
-  tabs.addEventListener('core-select',function(){
-    pages.selected = tabs.selected;
-  });
+  if (tabs) {
+      tabs.addEventListener('core-select',function(){
+        pages.selected = tabs.selected;
+      });
+  }
+  
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
